026fa786102eb98a53476c1116554ad360810c49
Gongmeda <gongmeda@gmail.com>
2023-04-19 21:31:51 +0900

    fix: CORS 오류 임시 해결

---
+++ api/src/main/java/com/gongmeda/ktechfeedbackend/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.gongmeda.ktechfeedbackend.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${spring.security.cors.allowed-origins}")
+    private String[] allowedOrigins;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins(allowedOrigins)
+            .allowedMethods("GET", "POST", "PATCH", "DELETE", "HEAD", "OPTIONS")
+            .allowCredentials(true)
+            .maxAge(3600);
+    }
+}
