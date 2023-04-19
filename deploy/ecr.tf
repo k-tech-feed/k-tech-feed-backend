791985d72ebf99a5c75376e7fa84884fd5e28c76
Gongmeda <gongmeda@gmail.com>
2023-04-19 15:45:45 +0900

    feat: crawler ci 추가

--- deploy/ecr.tf
+++ deploy/ecr.tf
@@ -1,5 +1,13 @@
-resource "aws_ecr_repository" "default" {
-  name                 = "${var.project_name}-ecr"
+resource "aws_ecr_repository" "api" {
+  name                 = "${var.project_name}-api"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "crawler" {
+  name                 = "${var.project_name}-crawler"
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = true
