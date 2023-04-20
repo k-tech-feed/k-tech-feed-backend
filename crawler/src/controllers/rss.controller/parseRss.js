fda23380dc675489cae5fa931b4c20f6867eb730
Yangseungchan <uvzone@naver.com>
2023-04-20 10:20:44 +0900

    feat: 이미지 추출 기능 추가

--- crawler/src/controllers/rss.controller/parseRss.js
+++ crawler/src/controllers/rss.controller/parseRss.js
@@ -1,50 +1,68 @@
 import Parser from "rss-parser";
+import { parse } from 'node-html-parser';
 import { gptRequest } from "../../utils/gptSummarize.js";
 
 const parser = new Parser();
 
 const RSS_REGEXP = /(<([^>]+)>|\&nbsp;)/gi;
 
+// feed 크롤링
 const fetchFeed = async (id, url) => {
   return { ...(await parser.parseURL(url)), id };
 };
 
-const summarizeFeedItem = async (feedItem) => {
-  const content = feedItem.content;
-
+// hashtag 추가
+const addHashtags = async (feedItem) => {
+  const summary = feedItem.summary;
   try {
-    const gptResponse = await gptRequest(content);
-    const summarizedContent = gptResponse.data.choices[0].message.content;
+    const gptResponse = await gptRequest(summary);
+    const gptContent = gptResponse.data.choices[0].message.content;
+    const hashtags = JSON.parse(gptContent).keywords;
 
     return {
       ...feedItem,
-      content: JSON.parse(JSON.stringify(summarizedContent)),
+      hashtags,
     };
   } catch (err) {
     console.log("gpt 에러 발생! : ", err);
     return {
       ...feedItem,
+      hashtags: feedItem.categories,
     };
   }
 };
+
+// 썸네일 이미지 추출
+const getThumbnailImg = (content) => {
+  const domRoot = parse(content);
+  const allImgNodes = domRoot.querySelectorAll('img');
+  const imgNodes = allImgNodes ? domRoot.querySelectorAll('img')?.filter(({attributes}) => !attributes.src.includes('emoji')) : null;
+  return imgNodes.length > 0 ? imgNodes[0].attributes.src : '';
+}
 
 const parseRss = async (req, res) => {
   const rssRequests = JSON.parse(JSON.stringify(req.body));
 
   // feed불러오기
   const feeds = await Promise.all(rssRequests.map(async ({ id, rssUrl }) => fetchFeed(id, rssUrl)));
-
+  
   // 파싱 후 아티클들의 배열로 만들기
   const feedItems = JSON.parse(JSON.stringify(feeds))
     .map((feed) => {
       return feed.items.map((item) => {
+        const content = item.hasOwnProperty('content:encoded') ? item['content:encoded'] : item.content;
+        const domRoot = parse(content);
+        const allImgNodes = domRoot.querySelectorAll('img');
+        const imgNodes = allImgNodes ? domRoot.querySelectorAll('img')?.filter(({attributes}) => !attributes.src.includes('emoji')) : null;
+
         return {
-          id: feed.id,
+          authorId: feed.id,
           title: item.title,
-          content: item.content.replace(RSS_REGEXP, "").slice(0, 1000),
-          pubDate: item.pubDate,
+          summary: content.replace(RSS_REGEXP, "").slice(0, 1000),
+          thumbnailUrl: getThumbnailImg(content),
+          linkUrl: item.link,
           categories: item.categories,
-          link: item.link,
+          timestamp: item.pubDate,
         };
       });
     })
@@ -52,14 +70,13 @@ const parseRss = async (req, res) => {
 
   for (let i = 0; i < feedItems.length; i++) {
     setTimeout(async () => {
-      const summarized = await summarizeFeedItem(feedItems[i]);
+      // hashtag 추가된 결과물
+      const newContent = await addHashtags(feedItems[i]);
       // summarized로 api 요청 보내기
-      console.log("summarize : ", summarized.content);
+      console.log("newContent : ", newContent);
     }, 20000 * i);
   }
 
-  //   const summarized = await summarizeFeedItem(feedItems[0]);
-  //   console.log(summarized);
 
   res.status(200).json({
     ...feedItems,
